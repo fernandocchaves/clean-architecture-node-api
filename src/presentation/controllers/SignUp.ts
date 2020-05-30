463ec3a027b0c14d3562e098da190866facf38fc
Fernando Cezar chaves <fernandocchaves@gmail.com>
2020-05-29 23:41:15 -0300

    chore: ensure SignUpController returns an error if no password confirmation and controller interface

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -1,10 +1,16 @@
-import { HttpRequest, HttpResponse } from '../protocols/http';
+import { HttpRequest, HttpResponse } from '../protocols/Http';
 import { MissingParamError } from '../errors/MissingParamError';
 import { BadRequest } from '../helpers/HttpHelpers';
+import { Controller } from '../protocols/Controller';
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password'];
+    const requiredFields = [
+      'name',
+      'email',
+      'password',
+      'passwordConfirmation',
+    ];
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
