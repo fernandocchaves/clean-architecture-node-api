5e60ff8e99c8d01dd266a2036d7889a1e6349a3c
Fernando Cezar chaves <fernandocchaves@gmail.com>
2020-05-29 23:07:05 -0300

    refactor: ensure SignUpController.handle implement interfaces and move duplicated code to helper

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -1,13 +1,15 @@
 import { HttpRequest, HttpResponse } from '../protocols/http';
+import { MissingParamError } from '../errors/MissingParamError';
+import { BadRequest } from '../helpers/HttpHelpers';
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return { statusCode: 400, body: new Error('Missing param: name') };
+      return BadRequest(new MissingParamError('name'));
     }
 
     if (!httpRequest.body.email) {
-      return { statusCode: 400, body: new Error('Missing param: email') };
+      return BadRequest(new MissingParamError('email'));
     }
   }
 }
