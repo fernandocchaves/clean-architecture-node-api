f230e9a26a6186f7a3b3d21297b1f89e72967051
Fernando Cezar chaves <fernandocchaves@gmail.com>
2020-06-03 00:15:39 -0300

    feat: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/SignUp.spec.ts
+++ src/presentation/controllers/signup/SignUp.spec.ts
@@ -26,7 +26,7 @@ const makeAddAccountStub = (): AddAccount => {
     add(account: AddAccountModel): AccountModel {
       const fakeAccount = {
         id: 'valid_id',
-        name: 'valid name',
+        name: 'valid_name',
         email: 'valid_email@email.com',
         password: 'valid_password',
       };
@@ -232,5 +232,27 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
+  });
+
+  test('Should return 200 if valid data is provided', () => {
+    const { sut } = makeSut();
+
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'invalid_email@mail.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'valid_password',
+    });
   });
 });
