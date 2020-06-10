dee7b6ac0b929da9b86f73ab3ec57ebe96213c55
Fernando Cezar chaves <fernandocchaves@gmail.com>
2020-06-10 02:29:47 -0300

    feat: ensure requests parse body as json

---
+++ src/main/middlewares/BodyParser.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest';
+import app from '../config/app';
+
+describe('BodyParserMiddleware', () => {
+  test('Should parser body as json', async () => {
+    app.post('/test-body-parser', (req, res) => {
+      res.send(req.body);
+    });
+
+    await request(app)
+      .post('/test-body-parser')
+      .send({ name: 'Fernando' })
+      .expect({ name: 'Fernando' });
+  });
+});
