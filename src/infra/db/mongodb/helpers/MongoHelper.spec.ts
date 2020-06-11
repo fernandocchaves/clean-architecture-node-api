1c24e657fa757b6db20ee9a313ac6c8e70ae3e23
Fernando Cezar chaves <fernandocchaves@gmail.com>
2020-06-11 01:03:25 -0300

    feat: ensure mongodb reconnec if client is down

---
+++ src/infra/db/mongodb/helpers/MongoHelper.spec.ts
@@ -0,0 +1,19 @@
+import { MongoHelper as sut } from './MongoHelper';
+
+describe('MongoHelper', () => {
+  beforeAll(async () => {
+    await sut.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await sut.disconnect();
+  });
+  test('Should reconnect if mongodb is down', async () => {
+    let accountConlection = await sut.getCollection('accounts');
+    expect(accountConlection).toBeTruthy();
+    await sut.disconnect;
+
+    accountConlection = await sut.getCollection('accounts');
+    expect(accountConlection).toBeTruthy();
+  });
+});
