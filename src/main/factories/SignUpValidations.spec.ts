5bd8bf12445596ef40f6e5c49421c05bf3b710af
Fernando Cezar chaves <fernandocchaves@gmail.com>
2020-06-14 04:17:48 -0300

    refactor: move password comparation  to ValidationComposite

--- src/main/factories/SignUpValidations.spec.ts
+++ src/main/factories/SignUpValidations.spec.ts
@@ -2,6 +2,7 @@ import { makeSignUpValidation } from './SignUpValidations';
 import { ValidationComposite } from '../../presentation/helpers/validations/ValidationComposite';
 import { RequiredFieldValidation } from '../../presentation/helpers/validations/RequiredFieldValidation';
 import { Validation } from '../../presentation/helpers/validations/Validation';
+import { CompareFieldValidation } from '../../presentation/helpers/validations/CompareFieldValidation';
 
 jest.mock('../../presentation/helpers/validations/ValidationComposite');
 
@@ -15,6 +16,9 @@ describe('SignUpValidation', () => {
       validations.push(new RequiredFieldValidation(field));
     }
 
+    validations.push(
+      new CompareFieldValidation('password', 'passwordConfirmation'),
+    );
     expect(ValidationComposite).toHaveBeenCalledWith(validations);
   });
 });
--- src/main/factories/SignUpValidations.ts
+++ src/main/factories/SignUpValidations.ts
@@ -1,6 +1,7 @@
 import { ValidationComposite } from '../../presentation/helpers/validations/ValidationComposite';
 import { RequiredFieldValidation } from '../../presentation/helpers/validations/RequiredFieldValidation';
 import { Validation } from '../../presentation/helpers/validations/Validation';
+import { CompareFieldValidation } from '../../presentation/helpers/validations/CompareFieldValidation';
 
 export const makeSignUpValidation = (): ValidationComposite => {
   const validations: Validation[] = [];
@@ -8,6 +9,8 @@ export const makeSignUpValidation = (): ValidationComposite => {
   for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
     validations.push(new RequiredFieldValidation(field));
   }
-
+  validations.push(
+    new CompareFieldValidation('password', 'passwordConfirmation'),
+  );
   return new ValidationComposite(validations);
 };
