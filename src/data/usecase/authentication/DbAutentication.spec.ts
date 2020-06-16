7bed626142e837029e2f529e7e7f6b8659013585
Fernando Cezar chaves <fernandocchaves@gmail.com>
2020-06-16 02:36:43 -0300

    test: ensure DbAutenthication returns null if LoadAccountByEmailRepository returns null

--- src/data/usecase/authentication/DbAutentication.spec.ts
+++ src/data/usecase/authentication/DbAutentication.spec.ts
@@ -61,4 +61,14 @@ describe('DBAuthentication', () => {
 
     await expect(promise).rejects.toThrow();
   });
+
+  test('Should return null if LoadAccountByEmailRepository returns null', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut();
+    jest
+      .spyOn(loadAccountByEmailRepositoryStub, 'load')
+      .mockReturnValueOnce(null);
+
+    const accessToken = await sut.auth(makeFakeAuthentication());
+    expect(accessToken).toBeNull();
+  });
 });
