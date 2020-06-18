c32d2aa0112e73c72eba7bb71b1c5c7d69379215
Fernando Cezar chaves <fernandocchaves@gmail.com>
2020-06-18 10:24:46 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success and fail

--- src/infra/db/mongodb/account-repository/Account.spec.ts
+++ src/infra/db/mongodb/account-repository/Account.spec.ts
@@ -1,6 +1,8 @@
 import { MongoHelper } from '../helpers/MongoHelper';
 import { AccountMongoRepository } from './Account';
+import { Collection } from 'mongodb';
 
+let accountCollection: Collection;
 const makeSut = (): AccountMongoRepository => {
   return new AccountMongoRepository();
 };
@@ -15,11 +17,11 @@ describe('Account Mongo Repository', () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts');
+    accountCollection = await MongoHelper.getCollection('accounts');
     await accountCollection.deleteMany({});
   });
 
-  test('Should return an account on success', async () => {
+  test('Should return an account on add success', async () => {
     const sut = makeSut();
 
     const account = await sut.add({
@@ -33,5 +35,29 @@ describe('Account Mongo Repository', () => {
     expect(account.name).toBe('any_name');
     expect(account.email).toBe('any_email@mail.com');
     expect(account.password).toBe('any_password');
+  });
+
+  test('Should return an account on loadByEmail success', async () => {
+    const sut = makeSut();
+
+    await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+    const account = await sut.loadByEmail('any_email@mail.com');
+
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe('any_name');
+    expect(account.email).toBe('any_email@mail.com');
+    expect(account.password).toBe('any_password');
+  });
+
+  test('Should return null if loadByEmail fails', async () => {
+    const sut = makeSut();
+    const account = await sut.loadByEmail('any_email@mail.com');
+
+    expect(account).toBeFalsy();
   });
 });
