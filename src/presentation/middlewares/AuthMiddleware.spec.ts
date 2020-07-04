aa2977aaa0fc388f7fca2117c98d5ec778beefa0
Fernando Cezar chaves <fernandocchaves@gmail.com>
2020-07-03 22:25:47 -0300

    feat: ensure AuthMiddleware return 500 if LoadAccountByToken throws

--- src/presentation/middlewares/AuthMiddleware.spec.ts
+++ src/presentation/middlewares/AuthMiddleware.spec.ts
@@ -1,5 +1,5 @@
 import { AccessDeniedError } from '../errors';
-import { forbidden, ok } from '../helpers/http/HttpHelpers';
+import { forbidden, ok, serverError } from '../helpers/http/HttpHelpers';
 import { AuthMiddleware } from './AuthMiddleware';
 import { AccountModel } from '../../domain/models/Account';
 import { LoadAccountByToken } from '../../domain/usecases/LoadAccountByToken';
@@ -73,5 +73,17 @@ describe('AuthMiddleware', () => {
 
     const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(ok({ account_id: 'valid_id' }));
+  });
+
+  test('Should return 500 if LoadAccountByToken throws', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut();
+    jest
+      .spyOn(loadAccountByTokenStub, 'load')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(serverError(new Error()));
   });
 });
--- src/presentation/middlewares/AuthMiddleware.ts
+++ src/presentation/middlewares/AuthMiddleware.ts
@@ -1,21 +1,25 @@
 import { Middleware, HttpRequest, HttpResponse } from '../protocols';
 import { AccessDeniedError } from '../errors';
-import { forbidden, ok } from '../helpers/http/HttpHelpers';
+import { forbidden, ok, serverError } from '../helpers/http/HttpHelpers';
 import { LoadAccountByToken } from '../../domain/usecases/LoadAccountByToken';
 
 export class AuthMiddleware implements Middleware {
   constructor(private readonly loadAccountByToken: LoadAccountByToken) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httpRequest.headers?.['x-access-token'];
+    try {
+      const accessToken = httpRequest.headers?.['x-access-token'];
 
-    if (accessToken) {
-      const account = await this.loadAccountByToken.load(accessToken);
-      if (account) {
-        return ok({ account_id: account.id });
+      if (accessToken) {
+        const account = await this.loadAccountByToken.load(accessToken);
+        if (account) {
+          return ok({ account_id: account.id });
+        }
       }
-    }
 
-    return forbidden(new AccessDeniedError());
+      return forbidden(new AccessDeniedError());
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
