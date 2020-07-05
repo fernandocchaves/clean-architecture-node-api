b6a739adf13ac7b79643526d3e28b56a042967a4
Fernando Cezar chaves <fernandocchaves@gmail.com>
2020-07-04 21:19:29 -0300

    feat: ensure DbLoadAccountByToken return an account on success

--- src/data/usecase/load-account-by-token/DbLoadAccountByToken.spec.ts
+++ src/data/usecase/load-account-by-token/DbLoadAccountByToken.spec.ts
@@ -87,4 +87,10 @@ describe('LoadAccountByToken Usecade', () => {
     const account = await sut.load('any_token', 'any_role');
     expect(account).toBeNull();
   });
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut();
+    const account = await sut.load('any_token', 'any_role');
+    expect(account).toEqual(makeFakeAccount());
+  });
 });
