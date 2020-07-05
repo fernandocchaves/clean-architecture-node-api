b3615f32d89a4b3549dbec3abca83aeb0375a898
Fernando Cezar chaves <fernandocchaves@gmail.com>
2020-07-04 21:29:15 -0300

    feat: ensure DbLoadAccountByToken throw if Decrypter throws

--- src/data/usecase/load-account-by-token/DbLoadAccountByToken.spec.ts
+++ src/data/usecase/load-account-by-token/DbLoadAccountByToken.spec.ts
@@ -93,4 +93,15 @@ describe('LoadAccountByToken Usecade', () => {
     const account = await sut.load('any_token', 'any_role');
     expect(account).toEqual(makeFakeAccount());
   });
+
+  test('Should throw if Decrypter throws', async () => {
+    const { sut, decrypterStub } = makeSut();
+    jest
+      .spyOn(decrypterStub, 'decrypt')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+    const promise = sut.load('any_token', 'any_role');
+    await expect(promise).rejects.toThrow();
+  });
 });
