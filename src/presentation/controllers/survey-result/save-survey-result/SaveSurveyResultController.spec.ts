4d6827580fe504f314fc53ffd07a7394d8ddd46e
Fernando Cezar chaves <fernandocchaves@gmail.com>
2020-08-29 13:26:00 -0300

    feat: ensure SaveSurveyResultController return 403 if an invalid answer is provided

--- src/presentation/controllers/survey-result/save-survey-result/SaveSurveyResultController.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-result/SaveSurveyResultController.spec.ts
@@ -14,6 +14,9 @@ const makeFakeRequest = (): HttpRequest => ({
   params: {
     surveyId: 'any_survey_id',
   },
+  body: {
+    answer: 'any_answer',
+  },
 });
 
 const makeFakeSurvey = (): SurveyModel => ({
@@ -67,6 +70,19 @@ describe('SaveSurveyResultController', () => {
       .mockReturnValueOnce(new Promise(resolve => resolve(null)));
     const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')));
+  });
+
+  test('Should return 403 if an invalid answer is provided', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle({
+      params: {
+        surveyId: 'any_survey_id',
+      },
+      body: {
+        answer: 'wrong_answer',
+      },
+    });
+    expect(httpResponse).toEqual(forbidden(new InvalidParamError('answer')));
   });
 
   test('Should return 500 if LoadSurveyById throws', async () => {
