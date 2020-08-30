fbb865ada7f7b3eab2f5b1c7d2a9669445ef31e0
Fernando Cezar chaves <fernandocchaves@gmail.com>
2020-08-30 08:38:08 -0300

    refactor: remove duplication

--- src/data/usecase/account/add-account/DbAddAccount.spec.ts
+++ src/data/usecase/account/add-account/DbAddAccount.spec.ts
@@ -7,21 +7,13 @@ import {
 import {
   Hasher,
   AddAccountRepository,
-  AccountModel,
   LoadAccountByEmailRepository,
 } from './DbAddAccountProtocols';
-import { mockHasher, mockAddAccountRepository } from '@/data/test';
-
-const mockLoadAccountByEmailRepository = (): LoadAccountByEmailRepository => {
-  class LoadAccountByEmailRepositoryStub
-    implements LoadAccountByEmailRepository {
-    async loadByEmail(email: string): Promise<AccountModel> {
-      return new Promise(resolve => resolve(null));
-    }
-  }
-
-  return new LoadAccountByEmailRepositoryStub();
-};
+import {
+  mockHasher,
+  mockAddAccountRepository,
+  mockLoadAccountByEmailRepository,
+} from '@/data/test';
 
 type SutTypes = {
   sut: DbAddAccount;
@@ -34,6 +26,11 @@ const makeSut = (): SutTypes => {
   const hasherStub = mockHasher();
   const addAccountRepositoryStub = mockAddAccountRepository();
   const loadAccountByEmailRepositoryStub = mockLoadAccountByEmailRepository();
+
+  jest
+    .spyOn(loadAccountByEmailRepositoryStub, 'loadByEmail')
+    .mockReturnValue(new Promise(resolve => resolve(null)));
+
   const sut = new DbAddAccount(
     hasherStub,
     addAccountRepositoryStub,
