6c7ea413afe2e9b8a6bdee709b21e1dc72ec16be
Fernando Cezar chaves <fernandocchaves@gmail.com>
2020-09-03 06:33:10 -0300

    refactor: change save survey result return

--- src/infra/db/mongodb/survey-result/SurveyResultMongoRespository.spec.ts
+++ src/infra/db/mongodb/survey-result/SurveyResultMongoRespository.spec.ts
@@ -1,6 +1,6 @@
 import { MongoHelper } from '@/infra/db/mongodb/helpers/MongoHelper';
 import { SurveyResultMongoRespository } from './SurveyResultMongoRespository';
-import { Collection } from 'mongodb';
+import { Collection, ObjectID } from 'mongodb';
 import { SurveyModel } from '@/domain/models/Survey';
 import { AccountModel } from '@/domain/models/Account';
 
@@ -27,7 +27,7 @@ const makeSurvey = async (): Promise<SurveyModel> => {
     date: new Date(),
   });
 
-  return res.ops[0];
+  return MongoHelper.map(res.ops[0]);
 };
 
 const makeAccount = async (): Promise<AccountModel> => {
@@ -37,7 +37,7 @@ const makeAccount = async (): Promise<AccountModel> => {
     password: 'any_password',
   });
 
-  return res.ops[0];
+  return MongoHelper.map(res.ops[0]);
 };
 
 describe('Survey Mongo Repository', () => {
@@ -74,16 +74,18 @@ describe('Survey Mongo Repository', () => {
       });
 
       expect(surveyResult).toBeTruthy();
-      expect(surveyResult.id).toBeTruthy();
-      expect(surveyResult.answer).toBe(survey.answers[0].answer);
+      expect(surveyResult.surveyId).toEqual(survey.id);
+      expect(surveyResult.answers[0].answer).toBe(survey.answers[0].answer);
+      expect(surveyResult.answers[0].count).toBe(1);
+      expect(surveyResult.answers[0].percent).toBe(100);
     });
 
     test('Should update survey result if its not new', async () => {
       const survey = await makeSurvey();
       const account = await makeAccount();
-      const res = await surveyResultCollection.insertOne({
-        surveyId: survey.id,
-        accountId: account.id,
+      await surveyResultCollection.insertOne({
+        surveyId: new ObjectID(survey.id),
+        accountId: new ObjectID(account.id),
         answer: survey.answers[0].answer,
         date: new Date(),
       });
@@ -98,8 +100,10 @@ describe('Survey Mongo Repository', () => {
       });
 
       expect(surveyResult).toBeTruthy();
-      expect(surveyResult.id).toEqual(res.ops[0]._id);
-      expect(surveyResult.answer).toBe(survey.answers[1].answer);
+      expect(surveyResult.surveyId).toEqual(survey.id);
+      expect(surveyResult.answers[0].answer).toBe(survey.answers[1].answer);
+      expect(surveyResult.answers[0].count).toBe(1);
+      expect(surveyResult.answers[0].percent).toBe(100);
     });
   });
 });
--- src/infra/db/mongodb/survey-result/SurveyResultMongoRespository.ts
+++ src/infra/db/mongodb/survey-result/SurveyResultMongoRespository.ts
@@ -2,6 +2,7 @@ import { SaveSurveyResultRepository } from '@/data/protocols/db/survey-result/Sa
 import { SaveSurveyResultParams } from '@/domain/usecases/survey-result/SaveSurveyResult';
 import { SurveyResultModel } from '@/domain/models/SurveyResult';
 import { MongoHelper } from '../helpers/MongoHelper';
+import { ObjectID } from 'mongodb';
 
 export class SurveyResultMongoRespository
   implements SaveSurveyResultRepository {
@@ -9,10 +10,10 @@ export class SurveyResultMongoRespository
     const surveyResultCollection = await MongoHelper.getCollection(
       'surveyResults',
     );
-    const res = await surveyResultCollection.findOneAndUpdate(
+    await surveyResultCollection.findOneAndUpdate(
       {
-        surveyId: data.surveyId,
-        accountId: data.accountId,
+        surveyId: new ObjectID(data.surveyId),
+        accountId: new ObjectID(data.accountId),
       },
       {
         $set: {
@@ -22,10 +23,115 @@ export class SurveyResultMongoRespository
       },
       {
         upsert: true,
-        returnOriginal: false,
       },
     );
 
-    return res.value && MongoHelper.map(res.value);
+    const surveuResult = await this.loadBySurveyId(data.surveyId);
+    return surveuResult;
+  }
+
+  private async loadBySurveyId(surveyId: string): Promise<SurveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getCollection(
+      'surveyResults',
+    );
+    const query = surveyResultCollection.aggregate([
+      {
+        $match: {
+          surveyId: new ObjectID(surveyId),
+        },
+      },
+      {
+        $group: {
+          _id: 0,
+          data: {
+            $push: '$$ROOT',
+          },
+          count: {
+            $sum: 1,
+          },
+        },
+      },
+      {
+        $unwind: {
+          path: '$data',
+        },
+      },
+      {
+        $lookup: {
+          from: 'surveys',
+          foreignField: '_id',
+          localField: 'data.surveyId',
+          as: 'survey',
+        },
+      },
+      {
+        $unwind: {
+          path: '$survey',
+        },
+      },
+      {
+        $group: {
+          _id: {
+            surveyId: '$survey._id',
+            question: '$survey.question',
+            date: '$survey.date',
+            total: '$count',
+            answer: {
+              $filter: {
+                input: '$survey.answers',
+                as: 'item',
+                cond: {
+                  $eq: ['$$item.answer', '$data.answer'],
+                },
+              },
+            },
+          },
+          count: {
+            $sum: 1,
+          },
+        },
+      },
+      {
+        $unwind: {
+          path: '$_id.answer',
+        },
+      },
+      {
+        $addFields: {
+          '_id.answer.count': '$count',
+          '_id.answer.percent': {
+            $multiply: [
+              {
+                $divide: ['$count', '$_id.total'],
+              },
+              100,
+            ],
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            surveyId: '$_id.surveyId',
+            question: '$_id.question',
+            date: '$_id.date',
+          },
+          answers: {
+            $push: '$_id.answer',
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          surveyId: '$_id.surveyId',
+          question: '$_id.question',
+          date: '$_id.date',
+          answers: '$answers',
+        },
+      },
+    ]);
+    const surveyResult = await query.toArray();
+    return surveyResult?.length ? surveyResult[0] : null;
   }
 }
